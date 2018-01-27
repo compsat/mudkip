ec7f9e01aa4af555db4500e3e059f5906e4fde65
Mikee Jazmines <mikee.jazmines@gmail.com>
2018-01-27 21:48:12 +0800

    Created profile and settings page

--- mobile/app/src/main/java/com/mudkip/lakbay/MainActivity.java
+++ mobile/app/src/main/java/com/mudkip/lakbay/MainActivity.java
@@ -9,19 +9,41 @@ public class MainActivity extends AppCompatActivity {
 
     private FragmentTransaction mFragmentTransaction;
     private MainFragment mMainFragment;
+    private ProfileFragment mProfileFragment;
+    private SettingsFragment mSettingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, Main2Activity.class);
+//        startActivity(intent);
 
-//        mMainFragment = new MainFragment();
-//
-//        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        mFragmentTransaction.replace(R.id.content, mMainFragment);
-//        mFragmentTransaction.commit();
+        mMainFragment = new MainFragment();
+        mProfileFragment = new ProfileFragment();
+        mSettingsFragment = new SettingsFragment();
+
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.content, mMainFragment);
+        mFragmentTransaction.commit();
+    }
+
+    public void changeToProfile(){
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.content, mProfileFragment);
+        mFragmentTransaction.commit();
+    }
+
+    public void changeToMain(){
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.content, mMainFragment);
+        mFragmentTransaction.commit();
+    }
+
+    public void changeToSettings(){
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.content, mSettingsFragment);
+        mFragmentTransaction.commit();
     }
 }
--- mobile/app/src/main/java/com/mudkip/lakbay/MainFragment.java
+++ mobile/app/src/main/java/com/mudkip/lakbay/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainFragment extends Fragment {
 
@@ -16,7 +17,16 @@ public class MainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ImageView hamburger = view.findViewById(R.id.hamburgerImageView);
+        hamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) MainFragment.this.getActivity()).changeToProfile();
+            }
+        });
+
+        return view;
     }
 
 }
