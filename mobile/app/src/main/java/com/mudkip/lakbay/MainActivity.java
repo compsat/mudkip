8070efcdb3b08df6796d2b8e82cb23e8d93c38d9
Luis Ligunas <luisrainier.ligunas@gmail.com>
2018-01-27 18:19:57 +0800

    Made MainActivity call MainFragment and transferred contents of MainActivity to MainFragment

--- mobile/app/src/main/java/com/mudkip/lakbay/MainActivity.java
+++ mobile/app/src/main/java/com/mudkip/lakbay/MainActivity.java
@@ -1,16 +1,23 @@
 package com.mudkip.lakbay;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentTransaction mFragmentTransaction;
+    private MainFragment mMainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFragment = new MainFragment();
+
+        mFragmentTransaction = getSupportFragmentManager().beginTransaction();
+        mFragmentTransaction.replace(R.id.content, mMainFragment);
+        mFragmentTransaction.commit();
     }
-
-
-
 }
